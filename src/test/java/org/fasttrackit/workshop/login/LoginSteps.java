2290ffe0aec213aef3421435cc869907207f596a
airbog <airbog85@gmail.com>
2015-07-03 14:50:50 +0300

    Added first cucumber tests.

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -1,6 +1,9 @@
 package org.fasttrackit.workshop.login;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
@@ -27,4 +30,47 @@ public class LoginSteps extends TestBaseNative {
         element.sendKeys(email);
     }
 
+    @Given("^I access login page$")
+    public void I_access_login_page() throws Throwable {
+        System.out.println("Aici ar trebui sa deschid login page.");
+    }
+
+    @And("^I insert valid credentials$")
+    public void I_insert_valid_credentials() throws Throwable {
+        System.out.println("User + pass.");
+    }
+
+    @When("^I click login button$")
+    public void I_click_login_button() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I check if user was logged in$")
+    public void I_check_if_user_was_logged_in() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @And("^I insert invalid credentials$")
+    public void I_insert_invalid_credentials() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I expect invalid credentials message$")
+    public void I_expect_invalid_credentials_message() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^I open this url \"([^\"]*)\"$")
+    public void I_open_this_url(String url) {
+        driver.get(url);
+    }
+
+    @Then("^I send (\\d+) into search field$")
+    public void I_sent_into_search(int arg1) {
+        System.out.println("numarul este: " + arg1);
+    }
 }
--- src/test/resources/feature/workshop/login.feature
+++ src/test/resources/feature/workshop/login.feature
@@ -1,1 +1,18 @@
+Feature: Login
+
+  Scenario: Open any web page
+    Given I open this url "http://google.com"
+    Then I send 5 into search field
+
+  Scenario: Successful Login
+    Given I access login page
+    And I insert valid credentials
+    When I click login button
+    Then I check if user was logged in
+
+  Scenario: Login wrong credentials
+    Given I access login page
+    And I insert invalid credentials
+    When I click login button
+    Then I expect invalid credentials message
 
