beb658f0fca58a527024bf4879f1a4fb4060824f
airbog <airbog85@gmail.com>
2015-07-04 13:38:15 +0300

    Added successful login using testy.

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -4,27 +4,15 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import org.fasttrackit.util.TestBaseNative;
-import org.fasttrackit.workshop.pagefactory.login.LoginPage;
-import org.openqa.selenium.support.PageFactory;
+import org.fasttrackit.util.TestBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class LoginSteps extends TestBaseNative {
+public class LoginSteps extends TestBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
 
-    LoginPage loginPage;
-
-    public LoginSteps() {
-        initPage();
-    }
-
-    public void initPage() {
-        loginPage = PageFactory.
-                initElements(driver, LoginPage.class);
-    }
+    private LoginView loginView = new LoginView();
 
     @Given("^I access login page$")
     public void I_access_login_page() {
@@ -33,29 +21,29 @@ public class LoginSteps extends TestBaseNative {
 
     @And("^I insert \"([^\"]*)\"/\"([^\"]*)\" credentials$")
     public void I_insert_credentials(String emailValue, String passwordValue) {
-        loginPage.enterEmail(emailValue);
-        loginPage.enterPassword(passwordValue);
+        loginView.enterEmail(emailValue);
+        loginView.enterPassword(passwordValue);
     }
 
     @When("^I click login button$")
     public void I_click_login_button() {
-        loginPage.clickOnLoginButton();
+        loginView.clickOnLoginButton();
     }
 
     @Then("^I check if user was logged in$")
     public void I_check_if_user_was_logged_in() {
-        loginPage.checkLogoutButton();
+        loginView.checkLogoutButton();
     }
 
     @Then("^I expect \"([^\"]*)\" error message$")
     public void I_expect_message(String expectedMessage) {
-        loginPage.errorMessageShouldBePresent(expectedMessage);
+        loginView.errorMessageShouldBePresent(expectedMessage);
     }
 
     @When("^I enter \"([^\"]*)\"/\"([^\"]*)\" credentials$")
     public void I_enter_credentials(String expectedEmail, String expectedPassword) {
-        loginPage.enterEmail(expectedEmail);
-        loginPage.enterPassword(expectedPassword);
+        loginView.enterEmail(expectedEmail);
+        loginView.enterPassword(expectedPassword);
     }
 
 }
